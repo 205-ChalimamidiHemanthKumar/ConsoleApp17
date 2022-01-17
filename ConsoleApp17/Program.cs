87685efde93630c098c33bcb59da1b0f1d7b7a46
ACER <ACER@DESKTOP-1BR1UTS>
2022-01-17 11:45:02 +0530

    Add project files.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new CustomersContext())
+            {
+                List<CustomersDetails> customersDetails = context.CustomersDetails.ToList();
+                foreach (var customerDetailss in customersDetails)
+                {
+                    Console.WriteLine(customerDetailss.Location);
+                }
+                    
+
+            }
+        }
+    }
+}
